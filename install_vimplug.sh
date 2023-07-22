06947c92a837f63d6189356bc08af2cb34b98f32
Lee Garam <47494005+KnightChaser@users.noreply.github.com>
2023-07-23 03:32:52 +0900

    Create install_vimplug.sh

---
+++ install_vimplug.sh
@@ -0,0 +1,6 @@
+sudo curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)" 
+
+brew install node
